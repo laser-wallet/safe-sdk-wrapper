65f0a9e05767385079a15513f01be7cfebd24820
James Bond <rodrigoheitie@hotmail.com>
2022-10-18 17:54:42 +0200

    export everything

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-import { ethers } from "ethers";
+import { Safe } from "./Safe";
+import { Factory } from "./Factory";
+import * as constants from "./constants";
+import * as utils from "./utils";
+
+export { Safe, Factory, constants, utils };
